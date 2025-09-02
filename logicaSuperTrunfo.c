f67c144a2e99b4f2c878da73f41dddc069ada578
Felipe-Alves <rodralves2011@gmail.com>
2025-09-02 00:10:23 +0000

    instrucao-usuario-terceiro-commit

--- logicaSuperTrunfo.c
+++ logicaSuperTrunfo.c
@@ -29,3 +29,34 @@ void exibirCarta(CartaCidade carta) {
     // Imprime a quantidade de pontos turísticos
     printf("Pontos Turísticos: %d\n", carta.pontosTuristicos);
 }
+
+int main() {
+    // Declaração de duas variáveis do tipo CartaCidade para armazenar as cartas
+    CartaCidade carta1, carta2;
+
+    // --- Cadastro da Carta 1 ---
+    // Mensagem de instrução para o usuário
+    printf("--- Cadastro da Primeira Carta ---\n");
+    // Solicita e lê o estado da cidade
+    printf("Digite o estado: ");
+    scanf("%s", carta1.estado);
+    // Solicita e lê o código da carta
+    printf("Digite o código da carta: ");
+    scanf("%d", &carta1.codigoCarta);
+    // Solicita e lê o nome da cidade
+    printf("Digite o nome da cidade: ");
+    scanf("%s", carta1.nomeCidade);
+    // Solicita e lê a população da cidade
+    printf("Digite a população: ");
+    scanf("%lld", &carta1.populacao);
+    // Solicita e lê a área da cidade
+    printf("Digite a área (em km²): ");
+    scanf("%lf", &carta1.area);
+    // Solicita e lê o PIB da cidade
+    printf("Digite o PIB (em bilhões de R$): ");
+    scanf("%lld", &carta1.pib);
+    // Solicita e lê o número de pontos turísticos
+    printf("Digite o número de pontos turísticos: ");
+    scanf("%d", &carta1.pontosTuristicos);
+
+    
